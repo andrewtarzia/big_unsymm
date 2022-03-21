da96712aadc4bcec08842eee777fa76fb05060e8
Andrew Tarzia <andrew.tarzia@gmail.com>
2022-03-21 22:14:12 +0000

    WIP subsystem construction.

--- build_subsystems.py
+++ build_subsystems.py
@@ -14,9 +14,9 @@ import sys
 import os
 import stk
 
-from env_set import cage_path, calc_path, meta_path, liga_path
-from utilities import AromaticCNCFactory
+from env_set import cage_path, calc_path, meta_path
 from optimisation import optimisation_sequence
+from m6l6 import M6L6
 
 
 def main():
@@ -29,22 +29,37 @@ def main():
     else:
         pass
 
-    li_path = liga_path()
-    lig_bb = stk.BuildingBlock.init_from_file(
-        path=os.path.join(li_path, 'lig_opt.mol'),
-        functional_groups=(AromaticCNCFactory(), ),
+    lig_bb = stk.BuildingBlock(
+        smiles='C1=CC(=CC=C1Br)Br',
+        functional_groups=(stk.BromoFactory(), ),
     )
     me_path = meta_path()
-    corner_bb = stk.BuildingBlock.init_from_file(
-        path=os.path.join(me_path, 'meta_opt.mol'),
-        functional_groups=(
-            stk.SmartsFunctionalGroupFactory(
-                smarts='[#46]~[#7]#[#6]~[#1]',
-                bonders=(0, ),
-                deleters=(1, 2, 3),
-            ),
+    corner_bbs = {
+        'cis1m1p': stk.BuildingBlock.init_from_file(
+            path=os.path.join(me_path, 'cis1m1p_opt.mol'),
+            functional_groups=(stk.BromoFactory(), ),
         ),
-    )
+        'cis2m0p': stk.BuildingBlock.init_from_file(
+            path=os.path.join(me_path, 'cis2m0p_opt.mol'),
+            functional_groups=(stk.BromoFactory(), ),
+        ),
+        'cis0m2p': stk.BuildingBlock.init_from_file(
+            path=os.path.join(me_path, 'cis0m2p_opt.mol'),
+            functional_groups=(stk.BromoFactory(), ),
+        ),
+        'trans1m1p': stk.BuildingBlock.init_from_file(
+            path=os.path.join(me_path, 'trans1m1p_opt.mol'),
+            functional_groups=(stk.BromoFactory(), ),
+        ),
+        'trans2m0p': stk.BuildingBlock.init_from_file(
+            path=os.path.join(me_path, 'trans2m0p_opt.mol'),
+            functional_groups=(stk.BromoFactory(), ),
+        ),
+        'trans0m2p': stk.BuildingBlock.init_from_file(
+            path=os.path.join(me_path, 'trans0m2p_opt.mol'),
+            functional_groups=(stk.BromoFactory(), ),
+        ),
+    }
     _wd = cage_path()
     _cd = calc_path()
 
@@ -55,15 +70,71 @@ def main():
         os.mkdir(_cd)
 
     # Define series of topologies to build.
-    _init_opts = stk.MCHammer(target_bond_length=1.5, num_steps=500)
-
+    _init_opts = stk.MCHammer(target_bond_length=2.5, num_steps=1000)
     _topos = {
         # Triangles.
-        'tri1': {},
+        'tri1': {
+            'tg': stk.cage.M3L3Triangle(
+                corners={
+                    corner_bbs['cis0m2p']: (0, 1, 2)
+                },
+                linkers=lig_bb,
+                optimizer=_init_opts,
+            ),
+            'charge': 2*3,
+        },
+        'tri2': {
+            'tg': stk.cage.M3L3Triangle(
+                corners={
+                    corner_bbs['cis2m0p']: (0, 1, 2)
+                },
+                linkers=lig_bb,
+                optimizer=_init_opts,
+            ),
+            'charge': 2*3,
+        },
         # Squares.
-        'sqr1': {},
+        'sqr1': {
+            'tg': stk.cage.M4L4Square(
+                corners={
+                    corner_bbs['cis0m2p']: (0, 1, 2, 3)
+                },
+                linkers=lig_bb,
+                optimizer=_init_opts,
+            ),
+            'charge': 2*4,
+        },
+        'sqr2': {
+            'tg': stk.cage.M4L4Square(
+                corners={
+                    corner_bbs['cis2m0p']: (0, 1, 2, 3)
+                },
+                linkers=lig_bb,
+                optimizer=_init_opts,
+            ),
+            'charge': 2*4,
+        },
         # Hexagons.
-        'hex1': {},
+        'hex1': {
+            'tg': M6L6(
+                corners={
+                    corner_bbs['trans0m2p']: (0, 1, 2, 3, 4, 5)
+                },
+                linkers=lig_bb,
+                optimizer=_init_opts,
+            ),
+            'charge': 2*6,
+        },
+        'hex2': {
+            'tg': M6L6(
+                corners={
+                    corner_bbs['trans2m0p']: (0, 1, 2, 3, 4, 5)
+                },
+                linkers=lig_bb,
+                optimizer=_init_opts,
+            ),
+            'charge': 2*6,
+        },
     }
     # Build them all.
     for topo in _topos:
@@ -75,10 +146,14 @@ def main():
         logging.info(f'building {topo}')
         unopt_mol = stk.ConstructedMolecule(tg)
         unopt_mol.write(unopt_file)
-        continue
         if not os.path.exists(opt_file):
             logging.info(f'optimising {topo}')
-            opt_mol = optimisation_sequence(unopt_mol, charge=charge)
+            opt_mol = optimisation_sequence(
+                mol=unopt_mol,
+                name=topo,
+                charge=charge,
+                calc_dir=_cd,
+            )
             opt_mol.write(opt_file)
 
 
