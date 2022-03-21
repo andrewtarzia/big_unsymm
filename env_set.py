ee96281692702c3a8aa681f4b1cc088b40c66b7b
Andrew Tarzia <andrew.tarzia@gmail.com>
2022-03-21 20:07:50 +0000

    Update.

--- env_set.py
+++ env_set.py
@@ -18,9 +18,31 @@ def cage_path():
     return '/data/atarzia/projects/big_unsymm/cages/'
 
 
+def calc_path():
+    return '/data/atarzia/projects/big_unsymm/calculations/'
+
+
 def crest_path():
-    raise NotImplementedError()
-    return '/home/atarzia/software/crest/crest'
+    return '/data/atarzia/projects/big_unsymm/software/crest'
+
+
+def crest_conformer_settings(solvent=None):
+    return {
+        'conf_opt_level': 'crude',
+        'final_opt_level': 'extreme',
+        'charge': 0,
+        'no_unpaired_e': 0,
+        'max_runs': 1,
+        'calc_hessian': False,
+        'solvent': solvent,
+        'nc': 4,
+        'etemp': 300,
+        'keepdir': False,
+        'cross': True,
+        'md_len': None,
+        'ewin': 5,
+        'speed_setting': None,
+    }
 
 
 def gulp_path():
