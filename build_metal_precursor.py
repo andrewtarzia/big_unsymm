55e24d7043cbf92f6ecc36837eaf8a2a9c60c1ab
Andrew Tarzia <andrew.tarzia@gmail.com>
2022-03-22 18:53:39 +0000

    Use a simpler bidentate bb.

--- build_metal_precursor.py
+++ build_metal_precursor.py
@@ -13,10 +13,8 @@ import logging
 import sys
 import os
 import stk
-import stko
 
-from env_set import meta_path, calc_path, gulp_path
-from utilities import AromaticCNCFactory
+from env_set import meta_path, calc_path
 
 
 def main():
@@ -39,8 +37,11 @@ def main():
         os.mkdir(_cd)
 
     bidentate = stk.BuildingBlock(
-        smiles='C1=CC=NC(=C1)C2=CC=CC=N2',
-        functional_groups=(AromaticCNCFactory(), ),
+        smiles='NCCN',
+        functional_groups=(stk.PrimaryAminoFactory(
+            bonders=(1, ),
+            deleters=(),
+        ), ),
     )
     pd = stk.BuildingBlock(
         smiles='[Pd+2]',
@@ -52,37 +53,15 @@ def main():
     )
 
     meta_unopt = os.path.join(_wd, 'meta_unopt.mol')
-    meta_opt = os.path.join(_wd, 'meta_opt.mol')
 
     logging.info(f'building metal')
     sqpl_unopt = stk.ConstructedMolecule(
         topology_graph=stk.metal_complex.CisProtectedSquarePlanar(
             metals=pd,
             ligands=bidentate,
-            optimizer=stk.MCHammer(
-                num_steps=500,
-                target_bond_length=1.8,
-            ),
         ),
     )
     sqpl_unopt.write(meta_unopt)
-
-    if not os.path.exists(meta_opt):
-        CG = False
-        logging.info(f'UFF4MOF optimisation 1 of meta CG: {CG}')
-        gulp_opt = stko.GulpUFFOptimizer(
-            gulp_path=gulp_path(),
-            maxcyc=1000,
-            metal_FF={46: 'Pd4+2'},
-            metal_ligand_bond_order='',
-            output_dir=os.path.join(_cd, f'meta_gulp'),
-            conjugate_gradient=CG,
-        )
-        gulp_opt.assign_FF(sqpl_unopt)
-        sqpl_gulp = gulp_opt.optimize(mol=sqpl_unopt)
-        sqpl_gulp.write(meta_opt)
-    else:
-        sqpl_gulp = sqpl_unopt.with_structure_from_file(meta_opt)
 
 
 if __name__ == "__main__":
